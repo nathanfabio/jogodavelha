7838793e92cca947fc5525d493e74f5956a89198
Nathan Fabio <nathan_3_m@hotmail.com>
2023-01-20 21:00:12 -0300

    fisrt part

---
+++ script.js
@@ -0,0 +1,38 @@
+const cells = Array.from(document.querySelectorAll(".board div"));
+
+let currentPlayer = 'X';
+let gameOver = false;
+
+
+cells.forEach(cell => cell.addEventListener('click', handleClick));
+
+
+function handleClick() {
+    if (this.innerHTML !== '' || gameOver) return;
+    this.innerHTML = currentPlayer;
+    checkForWinner();
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+function checkForWinner() {
+    winningCombinations.forEach(combination => {
+        const [a, b, c] = combination;
+        if (cells[a].innerHTML === currentPlayer && cells[b].innerHTML === currentPlayer && cells[c].innerHTML === currentPlayer) {
+            alert(`${currentPlayer} is the winner`);
+            gameOver = true;
+            cells.forEach(cell => cell.removeEventListener('click', handleClick));
+            
+        }
+    })
+}
